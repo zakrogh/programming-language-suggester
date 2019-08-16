10d5d9a4cf726769f417b71a98d8fd8c4d461279
Zach Krogh <zakrogh@gmail.com>
2019-08-16 14:59:05 -0700

    toggle panels based on state of survey and display modal when 5th panel is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 //Back End
-var questionState = 0;
+var questionState = 1;
 var choices = new Array(5);
 
 var panelClicked = function(panelName){
-  choices[questionState] = panelName.includes("1");
-  questionState++;
+  choices[questionState - 1] = panelName.includes("1");
 }
 //Front End
+var displayNext = function(){
+  $(".state" + questionState).toggle();
+  questionState++;
+  $(".state" + questionState).fadeToggle();
+}
+var showResults = function(){
+  $(".panel").css("display", "none");
+  $(".modal").modal();
+}
+
 $(document).ready(function(){
   $(".panel").click(function(){
     var panelName = this.className;
     panelClicked(panelName);
-
-    alert(questionState);
+    if(questionState === 5){
+      calculateResults();
+      showResults();
+    }
+    displayNext();
   });
 });
