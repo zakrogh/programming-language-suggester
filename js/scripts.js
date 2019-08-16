3a89128bda8475e1c6aee23b6d1ad9602c26f45a
Zach Krogh <zakrogh@gmail.com>
2019-08-16 14:05:19 -0700

    add javascript logic for clicking survey panels

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 //Back End
-  
+var questionState = 0;
+var choices = new Array(5);
+
+var panelClicked = function(panelName){
+  choices[questionState] = panelName.includes("1");
+  questionState++;
+}
 //Front End
 $(document).ready(function(){
   $(".panel").click(function(){
+    var panelName = this.className;
+    panelClicked(panelName);
 
+    alert(questionState);
   });
 });
